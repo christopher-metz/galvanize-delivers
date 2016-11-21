5ce47cc726a4a18c52d01c6be336b6f2dd0283b9
Christopher Metz <cmetz7@gmail.com>
2016-11-21 08:57:51 -0800

    Update final

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
 (function() {
   'use strict';
-  $(".button-collapse").sideNav();
+  $('.button-collapse').sideNav();
 })();
--- js/order.js
+++ js/order.js
@@ -1,7 +1,67 @@
 (function() {
   'use strict';
-  $(".button-collapse").sideNav();
+  $('.button-collapse').sideNav();
 
-  
+  const $cartTableBody = $('tbody');
+  let subtotal = 0;
 
+  const makeTableRow = function($food, $cost) {
+    const $tr = $('<tr>');
+    const $tdFood = $('<td>');
+    const $tdCost = $('<td>');
+
+    $tdFood.text($food.text());
+    $tdCost.addClass('price right-align');
+    $tdCost.text($cost.text());
+    $tr.append($tdFood);
+    $tr.append($tdCost);
+
+    return $tr;
+  };
+
+  const shoppingCart = function($orderRow) {
+    $cartTableBody.append($orderRow);
+  };
+
+  const addToSubtotal = function($cost) {
+    const $subtotal = $('td.subtotal');
+    const $tax = $('td.tax');
+    const $total = $('td.total');
+    const tax = Number.parseFloat($tax.text().slice(1));
+
+    subtotal += Number.parseFloat($cost.text().slice(1));
+    $subtotal.text(`${'$'}${subtotal.toFixed(2)}`);
+    const total = (1 + tax / 100) * subtotal;
+
+    $total.text(`${'$'}${total.toFixed(2)}`);
+  };
+
+  const cardClickHandler = function(event) {
+    const $target = $(event.target);
+    const $food = $target.parent().prev().children(':first-child');
+    const $cost = $target.parent().prev().children(':last-child');
+    const $newCartRow = makeTableRow($food, $cost);
+
+    shoppingCart($newCartRow);
+    addToSubtotal($cost);
+  };
+
+  $('.cards').on('click', 'a', cardClickHandler);
+
+  $('#userinfo').on('submit', $('#placeorder'), (event) => {
+    event.preventDefault();
+    if ($cartTableBody.children().length === 0) {
+      Materialize.toast('You must select something to eat!', 4000);
+    }
+    else {
+      for (let i = 0; i < 3; i++) {
+        if ($('input')[i].value === '') {
+          Materialize.toast('All address fields must be filled.', 4000);
+
+          return;
+        }
+      }
+      Materialize.toast('Thank You! Your order has been placed!', 4000);
+    }
+  });
 })();
